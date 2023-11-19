c37966fb89e092c06e6fe4d56d0e32af6c09d4f6
BeAsT436 <yuriryabenko436@gmail.com>
2023-11-19 19:37:57 +0200

    fix code

---
+++ quizData.js
@@ -0,0 +1,93 @@
+export const rebusData = [
+    {
+      id: 'rebus1',
+      imagePath: './photo_2023-11-12_22-39-02.jpg',
+      correctAnswer: 'слово',
+    },
+    {
+      id: 'rebus2',
+      imagePath: './photo_2023-11-12_23-38-27.jpg',
+      correctAnswer: 'співак',
+    },
+    {
+      id: 'rebus3',
+      imagePath: './photo_2023-11-12_23-41-55.jpg',
+      correctAnswer: 'тривога',
+    },
+    {
+      id: 'rebus4',
+      imagePath: './photo_2023-11-12_23-44-57.jpg',
+      correctAnswer: 'серце',
+    },
+  ]
+  
+  export const yesNoData = [
+    {
+      id: 'question1',
+      questionText: '1. Найменше натуральне число нуль?',
+      correctAnswer: false,
+    },
+    {
+      id: 'question2',
+      questionText:
+        '2. Сума гострих кутів прямокутного трикутника дорівнює 90\u00B0?',
+      correctAnswer: true,
+    },
+    {
+      id: 'question3',
+      questionText:
+        '3. Піфагор до своєї школи активно залучав жінок тому, що вважав, що вони більш здібні до математики, ніж чоловіки?',
+      correctAnswer: false,
+    },
+    {
+      id: 'question4',
+      questionText:
+        '4. Брати Гримм, автори казки «Бременські музиканти», написали Казку «Цікаві пригоди трикутника»?',
+      correctAnswer: true,
+    },
+    {
+      id: 'question5',
+      questionText:
+        '5. Шарль Перро, автор «Червоної шапочки», написав казку «Любов циркуля та лінійки»?',
+      correctAnswer: false,
+    },
+  ]
+  
+  export const textQuestionData = [
+    {
+      id: 'q1',
+      questionText: 'Син з батьком та дід з онуком. Скільки їх?',
+      correctAnswer: '3',
+    },
+    {
+      id: 'q2',
+      questionText: 'Горіло 5 свічок. Дві погасли. Скільки залишилось?',
+      correctAnswer: '2',
+    },
+    {
+      id: 'q3',
+      questionText: 'Який кут опише годинна стрілка за 2 години?',
+      correctAnswer: '60',
+    },
+    {
+      id: 'q4',
+      questionText: 'Що йде, не рухаючись з місця?',
+      correctAnswer: 'час',
+    },
+    {
+      id: 'q5',
+      questionText:
+        'Як, не користуючись дією додавання, збільшити число 86 на 12?',
+      correctAnswer: 'перевернути його на 180 градусів',
+    },
+    {
+      id: 'q6',
+      questionText: 'Скільки буде  6² + 6 : 6 = ?',
+      correctAnswer: '37',
+    },
+    {
+      id: 'q7',
+      questionText: 'Скільки двоцифрових чисел, у яких перша цифра 1?',
+      correctAnswer: '10',
+    },
+  ]
--- script.js
+++ script.js
@@ -1,200 +1,165 @@
-// ребуси
-const input = document.querySelector("#input")
-const btn = document.querySelector("#btn")
-const paragraph = document.querySelector(".paragraph")
-            
-btn.addEventListener("click", ()=>{
-    if (input.value == "слово") {
-        paragraph.textContent = "Правильно!!!"
-        input.value = ""
-    }
-    else{
-        paragraph.textContent = "Спробуй ще раз"
-        input.value = ""
-    }
-})
+import { rebusData, yesNoData, textQuestionData } from './quizData.js'
 
-const input2 = document.querySelector("#input2")
-const btn2 = document.querySelector("#btn2")
-const p2 = document.querySelector("#p2")
-            
-btn2.addEventListener("click", ()=>{
-    if (input2.value == "співак") {
-        p2.textContent = "Правильно!!!"
-        input2.value = ""
-    }
-    else{
-        p2.textContent = "Спробуй ще раз"
-        input2.value = ""
-    }
-})
+// створення ребусів
 
-const input3 = document.querySelector("#input3")
-const btn3 = document.querySelector("#btn3")
-const p3 = document.querySelector("#p3")
-            
-btn3.addEventListener("click", ()=>{
-    if (input3.value == "тривога") {
-        p3.textContent = "Правильно!!!"
-        input3.value = ""
-    }
-    else{
-        p3.textContent = "Спробуй ще раз"
-        input3.value = ""
-    }
-})
+function createRebus(id, imagePath, correctAnswer) {
+  const rebusContainer = document.getElementById('rebus-container')
 
-const input4 = document.querySelector("#input4")
-const btn4 = document.querySelector("#btn4")
-const p4 = document.querySelector("#p4")
-            
-btn4.addEventListener("click", ()=>{
-    if (input4.value == "серце") {
-        p4.textContent = "Правильно!!!"
-        input4.value = ""
-    }
-    else{
-        p4.textContent = "Спробуй ще раз"
-        input4.value = ""
-    }
-})
-// кінець ребусів
-// Так чи ні
-const button = document.querySelector(".btn")
-const button2 = document.querySelector(".btn2")
-const button3 = document.querySelector(".btn3")
-const button4 = document.querySelector(".btn4")
-const button5 = document.querySelector(".btn5")
-const button6 = document.querySelector(".btn6")
-const button7 = document.querySelector(".btn7")
-const button8 = document.querySelector(".btn8")
-const button9 = document.querySelector(".btn9")
-const button10 = document.querySelector(".btn10")
+  const rebusDiv = document.createElement('div')
+  rebusDiv.classList.add('rebus')
+  rebusDiv.id = id
 
-const p = document.querySelector(".p1")
-const p_2 = document.querySelector(".p2")
-const p_3 = document.querySelector(".p3")
-const p_4 = document.querySelector(".p4")
-const p_5 = document.querySelector(".p5")
+  const imageContainer = document.createElement('div')
+  imageContainer.classList.add('image-container')
 
-button.addEventListener("click", ()=>{
-    p.textContent = "Неправильно"
-})
-button2.addEventListener("click", ()=>{
-    p.textContent = "Правильно!!!"
-})
-button3.addEventListener("click", ()=>{
-    p_2.textContent = "Правильно!!!"
-})
-button4.addEventListener("click", ()=>{
-    p_2.textContent = "Неправильно"
-})
-button5.addEventListener("click", ()=>{
-    p_3.textContent = "Неправильно"
-})
-button6.addEventListener("click", ()=>{
-    p_3.textContent = "Правильно!!!"
-})
-button7.addEventListener("click", ()=>{
-    p_4.textContent = "Неправильно"
-})
-button8.addEventListener("click", ()=>{
-    p_4.textContent = "Правильно!!!"
-})
-button9.addEventListener("click", ()=>{
-    p_5.textContent = "Правильно!!!"
-})
-button10.addEventListener("click", ()=>{
-    p_5.textContent = "Неправильно"
-})
-// кінець так чи ні
-// Дай відповідь на запитання
-const inp1 = document.querySelector(".inp1")
-const inp2 = document.querySelector(".inp2")
-const inp3 = document.querySelector(".inp3")
-const inp4 = document.querySelector(".inp4")
-const inp5 = document.querySelector(".inp5")
-const inp6 = document.querySelector(".inp6")
-const inp7 = document.querySelector(".inp7")
+  const image = document.createElement('img')
+  image.classList.add('fill-image')
+  image.src = imagePath
 
-const btn_1 = document.querySelector("#btn_1")
-const btn_2 = document.querySelector("#btn_2")
-const btn_3 = document.querySelector("#btn_3")
-const btn_4 = document.querySelector("#btn_4")
-const btn_5 = document.querySelector("#btn_5")
-const btn_6 = document.querySelector("#btn_6")
-const btn_7 = document.querySelector("#btn_7")
+  const input = document.createElement('input')
+  input.type = 'text'
 
-const p__1 = document.querySelector("#p_1")
-const p__2 = document.querySelector("#p_2")
-const p__3 = document.querySelector("#p_3")
-const p__4 = document.querySelector("#p_4")
-const p__5 = document.querySelector("#p_5")
-const p__6 = document.querySelector("#p_6")
-const p__7 = document.querySelector("#p_7")
+  const button = document.createElement('button')
+  button.textContent = 'ввести'
+  button.addEventListener('click', () =>
+    checkAnswer(id, input.value, correctAnswer)
+  )
 
-btn_1.addEventListener("click", ()=>{
-    if (inp1.value == "3") {
-        p__1.textContent = "Правильно!!!"
-        inp1.value = ""
-    }else{
-        p__1.textContent = "Спробуй ще раз"
-        inp1.value = ""
-    }
-})
-btn_2.addEventListener("click", ()=>{
-    if (inp2.value == "2") {
-        p__2.textContent = "Правильно!!!"
-        inp2.value = ""
-    }else{
-        p__2.textContent = "Спробуй ще раз"
-        inp2.value = ""
-    }
-})
-btn_3.addEventListener("click", ()=>{
-    if (inp3.value == "60") {
-        p__3.textContent = "Правильно!!!"
-        inp3.value = ""
-    }else{
-        p__3.textContent = "Спробуй ще раз"
-        inp3.value = ""
-    }
-})
-btn_4.addEventListener("click", ()=>{
-    if (inp4.value == "час") {
-        p__4.textContent = "Правильно!!!"
-        inp4.value = ""
-    }else{
-        p__4.textContent = "Спробуй ще раз"
-        inp4.value = ""
-    }
-})
-btn_5.addEventListener("click", ()=>{
-    if (inp5.value == "перевернути його на 180 градусів") {
-        p__5.textContent = "Правильно!!!"
-        inp5.value = ""
-    }else{
-        p__5.textContent = "Спробуй ще раз"
-        inp5.value = ""
-    }
-})
-btn_6.addEventListener("click", ()=>{
-    if (inp6.value == "37") {
-        p__6.textContent = "Правильно!!!"
-        inp6.value = ""
-    }else{
-        p__6.textContent = "Спробуй ще раз"
-        inp6.value = ""
-    }
-})
-btn_7.addEventListener("click", ()=>{
-    if (inp7.value == "10") {
-        p__7.textContent = "Правильно!!!"
-        inp7.value = ""
-    }else{
-        p__7.textContent = "Спробуй ще раз"
-        inp7.value = ""
-    }
-})
-// кінець запитаннь
+  const paragraph = document.createElement('p')
+  paragraph.classList.add('paragraph')
 
+  imageContainer.appendChild(image)
+  rebusDiv.appendChild(imageContainer)
+  rebusDiv.appendChild(input)
+  rebusDiv.appendChild(button)
+  rebusDiv.appendChild(paragraph)
+
+  rebusContainer.appendChild(rebusDiv)
+}
+
+// кінець створення ребусів
+// створення запитань
+
+function createYesNoQuestion(id, questionText, correctAnswer) {
+  const yesNoContainer = document.getElementById('yes-or-no-container')
+
+  const questionDiv = document.createElement('div')
+  questionDiv.classList.add('question')
+  questionDiv.id = id
+
+  const questionParagraph = document.createElement('p')
+  questionParagraph.textContent = questionText
+
+  const buttonYes = document.createElement('button')
+  buttonYes.classList.add('btn')
+  buttonYes.textContent = 'Так'
+  buttonYes.addEventListener('click', () =>
+    checkYesNoAnswer(id, true, correctAnswer)
+  )
+
+  const buttonNo = document.createElement('button')
+  buttonNo.classList.add('btn2')
+  buttonNo.textContent = 'Ні'
+  buttonNo.addEventListener('click', () =>
+    checkYesNoAnswer(id, false, correctAnswer)
+  )
+
+  const paragraph = document.createElement('p')
+  paragraph.classList.add('p')
+
+  questionDiv.appendChild(questionParagraph)
+  questionDiv.appendChild(buttonYes)
+  questionDiv.appendChild(buttonNo)
+  questionDiv.appendChild(paragraph)
+
+  yesNoContainer.appendChild(questionDiv)
+}
+
+// кінець створення запитань
+// створення текстових запитань
+
+function createTextQuestion(id, questionText, correctAnswer) {
+  const questionsContainer = document.getElementById('questions-container')
+
+  const questionDiv = document.createElement('div')
+  questionDiv.classList.add('question')
+  questionDiv.id = id
+
+  const questionParagraph = document.createElement('p')
+  questionParagraph.textContent = questionText
+
+  const input = document.createElement('input')
+  input.type = 'text'
+
+  const button = document.createElement('button')
+  button.textContent = 'ввести'
+  button.addEventListener('click', () =>
+    checkTextQuestionAnswer(id, input.value, correctAnswer)
+  )
+
+  const paragraph = document.createElement('p')
+  const resultParagraph = document.createElement('p')
+
+  questionDiv.appendChild(questionParagraph)
+  questionDiv.appendChild(input)
+  questionDiv.appendChild(button)
+  questionDiv.appendChild(paragraph)
+  questionDiv.appendChild(resultParagraph)
+
+  questionsContainer.appendChild(questionDiv)
+}
+
+// кінець створення текстових запитань
+// перевірка відповідей
+
+function checkAnswer(id, userAnswer, correctAnswer) {
+  const paragraph = document.querySelector(`#${id} .paragraph`)
+  if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+    paragraph.textContent = 'Правильно!!!'
+    paragraph.style.color = 'green'
+  } else {
+    paragraph.textContent = 'Спробуй ще раз'
+    paragraph.style.color = 'red'
+  }
+}
+
+function checkYesNoAnswer(id, userAnswer, correctAnswer) {
+  const paragraph = document.querySelector(`#${id} .p`)
+  if (userAnswer === correctAnswer) {
+    paragraph.textContent = 'Правильно!!!'
+    paragraph.style.color = 'green'
+  } else {
+    paragraph.textContent = 'Неправильно'
+    paragraph.style.color = 'red'
+  }
+}
+
+function checkTextQuestionAnswer(id, userAnswer, correctAnswer) {
+  const paragraph = document.querySelector(`#${id} p`)
+  const resultParagraph = document.querySelector(`#${id} p + p`)
+
+  if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+    resultParagraph.textContent = 'Правильно!!!'
+    resultParagraph.style.color = 'green'
+  } else {
+    resultParagraph.textContent = 'Спробуй ще раз'
+    resultParagraph.style.color = 'red'
+  }
+}
+// кінець перевірки відповідей
+// ініціалізація елементів
+
+function initializeElements() {
+  rebusData.forEach((data) =>
+    createRebus(data.id, data.imagePath, data.correctAnswer)
+  )
+  yesNoData.forEach((data) =>
+    createYesNoQuestion(data.id, data.questionText, data.correctAnswer)
+  )
+  textQuestionData.forEach((data) =>
+    createTextQuestion(data.id, data.questionText, data.correctAnswer)
+  )
+}
+// кінець ініціалізування елементів
+// викликаємо ініціалізацію елементів
+initializeElements()
